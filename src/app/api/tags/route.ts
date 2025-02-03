9f97b00f9dc308ea754a875f40acfe3bdfd66f66
James Drake <info@jamesmdrakemedia.com>
2025-02-03 09:04:04 -0500

    chore: removed commented code

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -11,15 +11,6 @@ export function GET() {
     status = 200;
 
     // processing tags into a deduplicated list
-    /* let bigList: string[] = [];
-    for (let i = 0; i < body.length; i++) {
-      let tags: string[] = (body[i] as { post_tags: string }).post_tags.split(",");
-      tags = tags.map((tag) => tag.trim());
-      bigList = bigList.concat(tags);
-    }
-    const dedupe = new Set(bigList);
-    const tagList = [...dedupe]; */
-
     const tagCountMap = new Map<string, number>();
     for (let i = 0; i < body.length; i++) {
       let tags: string[] = (body[i] as { post_tags: string }).post_tags.split(",");
