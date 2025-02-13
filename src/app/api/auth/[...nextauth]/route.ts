5c756c51e62b22ef1b69921438f805db837c1913
James Drake <info@jamesmdrakemedia.com>
2025-02-13 02:58:18 -0500

    refactor: starting over

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,15 @@
-// library imports
-import { handlers } from "@/auth";
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
-export const { GET, POST } = handlers;
+export const authOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+  ],
+};
+
+export const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
