9be52a4feaa4acb5e528a82799c08743b7cda74f
James Drake <info@jamesmdrakemedia.com>
2025-02-19 15:34:33 -0500

    refactor: refactored posts by category and posts by tag

--- src/app/posts/category/[category]/page.tsx
+++ src/app/posts/category/[category]/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-import useGetPostList from "@/hooks/useGetPosts";
-import { useParams } from "next/navigation";
+import Category from "./Category";
 
 export default function PostsByCategory() {
-  const params = useParams<{ category: string }>();
-  const { posts, postLoading } = useGetPostList({ group: "category", option: params.category });
-
   return (
     <>
-      <h2>
-        Category: <span id="categoryName">{params.category}</span>
-      </h2>
-      {postLoading ? <span>Loading...</span> : posts}
+      <Category />
     </>
   );
 }
--- src/app/posts/tag/[tag]/page.tsx
+++ src/app/posts/tag/[tag]/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-import { useParams } from "next/navigation";
-import useGetPostList from "@/hooks/useGetPosts";
+import Tag from "./Tag";
 
 export default function PostsByTag() {
-  const params = useParams<{ tag: string }>();
-  const { posts, postLoading } = useGetPostList({ group: "tag", option: params.tag });
-
   return (
     <>
-      <h2>
-        Tag: <span id="tagName">{decodeURI(params.tag)}</span>
-      </h2>
-      {postLoading ? <span>Loading...</span> : posts}
+      <Tag />
     </>
   );
 }
