5a7daacff95947580a595ed9991e25419317f8ac
James Drake <info@jamesmdrakemedia.com>
2025-02-19 14:30:18 -0500

    refactor: refactored home page to use components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,5 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "James Drake - Developer",
-  description: "James Drake's Development Portfolio Website",
-};
-
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
