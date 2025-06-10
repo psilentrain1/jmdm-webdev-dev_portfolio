19e493529eb004d2f80b5306dc3de94ee568ce59
James Drake <info@jamesmdrakemedia.com>
2025-06-10 15:23:23 -0400

    feat: added portfolio link to hamburger menu on posts layout

--- src/components/PostComponents.tsx
+++ src/components/PostComponents.tsx
@@ -136,9 +136,9 @@ export function Hamburger() {
           <li>
             <Link href="/#about">About</Link>
           </li>
-          {/* <li>
-              <Link href="/#projects">Projects</Link>
-            </li> */}
+          <li>
+            <Link href="/portfolio">Portfolio</Link>
+          </li>
           <li>
             <Link href="/posts">Posts</Link>
           </li>
